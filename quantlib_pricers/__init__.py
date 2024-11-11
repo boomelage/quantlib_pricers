07950d9801b2de13ced924b41503ef7e769c8220
radusbriciu <radusbriciu@proton.me>
2024-11-11 12:21:26 +0000

    testing

--- quantlib_pricers/__init__.py
+++ quantlib_pricers/__init__.py
@@ -1,3 +1,6 @@
 from .vanilla_pricer import vanilla_pricer 
 from .asian_option_pricer import asian_option_pricer
 from .barrier_option_pricer import barrier_option_pricer
+vanillas = vanilla_pricer()
+barriers = barrier_option_pricer()
+asians = asian_option_pricer()
